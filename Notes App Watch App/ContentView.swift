7dee7447ff1d11585068f70f75ec77111a49a954
chiransiriwardhana <chiranchathwara16@gmail.com>
2024-04-19 03:20:11 +0530

    first commit

--- Notes App Watch App/ContentView.swift
+++ Notes App Watch App/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+
+    func save() {
+        // dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed")
         }
-        .padding()
+    }
+
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+
+            }
+        }
+    }
+
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+
+    var body: some View {
+
+        VStack {
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 6){
+                TextField("Add new note", text: $text)
+                Button {
+
+                    guard text.isEmpty == false else { return }
+
+                    let note = Note(id: UUID(), text: text)
+
+                    notes.append(note)
+
+                    text = ""
+
+                    save()
+
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 42, weight: .semibold))
+                }
+
+                .fixedSize()
+                .buttonStyle(PlainButtonStyle())
+                .foregroundColor(.accentColor)
+            }
+
+            Spacer()
+
+            List {
+                ForEach(0..<notes.count, id: \.self) { i in
+                    HStack {
+                        Capsule()
+                            .frame(width: 4)
+                            .foregroundColor(.accentColor)
+                        Text(notes[i].text)
+                            .lineLimit(1)
+                            .padding(.leading, 5)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Notes")
+        .onAppear(perform: {
+            load()
+        })
     }
 }
 
