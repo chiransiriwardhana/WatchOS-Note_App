0aac0d59a282e0ec8b807eaf27bd5de6b229b62f
chiransiriwardhana <chiranchathwara16@gmail.com>
2024-04-19 02:55:19 +0530

    Implement notes adding feature

---
+++ Notes App Watch App/Models/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  Notes App Watch App
+//
+//  Created by Chiran Siriwardena on 4/18/24.
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+
+    let id: UUID
+    let text: String
+
+}
---
+++ Notes App Watch App/Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppApp.swift
+//  Notes App Watch App
+//
+//  Created by Chiran Siriwardena on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
