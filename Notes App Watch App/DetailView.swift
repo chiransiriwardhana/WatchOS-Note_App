ae737be10877905c4cd705994daba8ded667737a
chiransiriwardhana <chiranchathwara16@gmail.com>
2024-04-19 18:38:44 +0530

    Add missing files

---
+++ Notes App Watch App/DetailView.swift
@@ -0,0 +1,58 @@
+//
+//  DetailView.swift
+//  Notes App Watch App
+//
+//  Created by Chiran Siriwardena on 4/19/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+
+    let note: Note
+    let count: Int
+    let index: Int
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 3) {
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+            }
+            .foregroundColor(.accentColor)
+
+            Spacer()
+
+            ScrollView(.vertical) {
+                Text(note.text)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
+
+            Spacer()
+
+            HStack(alignment: .center) {
+                Image(systemName: "gear")
+                    .imageScale(.large)
+                Spacer()
+                Text("\(count)/\(index+1)")
+                Spacer()
+
+                Image(systemName: "info.circle")
+                    .imageScale(.large)
+            }
+            .foregroundColor(.secondary)
+        }
+        .padding(3)
+    }
+}
+
+
+
+
+struct DetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    DetailView(note:Note(id: UUID(), text: "Hello Chiran"), count: 5, index: 1)
+  }
+}
