1cd34ef3fa317cb588e39a2a5106bf5e7539362e
chiransiriwardhana <chiranchathwara16@gmail.com>
2024-04-20 18:54:42 +0530

    Implement credit view

--- Notes App Watch App/CreditView.swift
+++ Notes App Watch App/CreditView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct CreditView: View {
+
+    @State private var randomNumber: Int = Int.random(in: 1..<4)
+
     var body: some View {
+        
         VStack(spacing: 3) {
+
+            Image("Star")
+                .resizable()
+                .scaledToFill()
+                .layoutPriority(1)
+            
             HeaderView(title: "Credits")
-            ////
+
+            Text("Chiran Siriwardena")
+                .foregroundColor(.primary)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+
+            Text("Developer")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .fontWeight(.light)
         }
     }
 }
--- Notes App Watch App/DetailView.swift
+++ Notes App Watch App/DetailView.swift
@@ -13,6 +13,8 @@ struct DetailView: View {
     let count: Int
     let index: Int
 
+    @State private var isCreditsPresented: Bool = false
+
     var body: some View {
         VStack(alignment: .center, spacing: 3) {
 
@@ -38,6 +40,12 @@ struct DetailView: View {
 
                 Image(systemName: "info.circle")
                     .imageScale(.large)
+                    .onTapGesture {
+                        isCreditsPresented.toggle()
+                    }
+                    .sheet(isPresented: $isCreditsPresented, content: {
+                        CreditView()
+                    })
             }
             .foregroundColor(.secondary)
         }
